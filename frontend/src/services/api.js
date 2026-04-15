d29f4c1d33974f0e628d234639da65eddf511f69
medhinibr <brmedhini@gmail.com>
2026-04-15 23:12:04 +0530

    Fix Authorization Bearer prefix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -16,7 +16,7 @@ api.interceptors.request.use(async (config) => {
   const user = auth.currentUser;
   if (user) {
     const token = await user.getIdToken();
-    config.headers.Authorization = token;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 }, (error) => {
